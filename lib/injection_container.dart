2ee3a5f58e55165f58f213df5def240ca35005e5
hentzrafael <rafaelhentz172@gmail.com>
2023-08-31 10:19:25 -0300

    Injection using get_it created

---
+++ lib/injection_container.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app/features/daily_news/data/repository/article_repository_impl.dart';
+import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
+import 'package:news_app/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+
+  sl.registerSingleton<Dio>(Dio());
+
+  //Dependencies
+  sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
+  sl.registerSingleton<GetArticleUseCase>(GetArticleUseCase(sl()));
+
+  //Blocs
+  sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/injection_container.dart';
 
 void main() {
+  initializeDependencies();
   runApp(const MyApp());
 }
 
