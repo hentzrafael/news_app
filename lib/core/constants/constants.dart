0a4f3ce82d063f4cab4776ce5266ec8f292dc653
hentzrafael <rafaelhentz172@gmail.com>
2023-08-31 07:51:02 -0300

    Repository implemented to request articles

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = 'baacbefa44464fc4ac4dfbf15540cf27';
+const String countryQuery = 'br';
+const String categoryQuery = 'business';
--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,10 +1,33 @@
+import 'dart:html';
+
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/constants.dart';
 import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app/features/daily_news/data/models/article.dart';
 import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository{
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles()async {
+    try{
+      final httpResponse = await _newsApiService.getNewsArticles(
+          apiKey: newsAPIKey, category: categoryQuery, country: countryQuery);
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions));
+      }
+    } on DioException catch (e){
+      return DataFailed(e);
+    }
   }
 }
